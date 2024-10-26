55c8a7eb6aac7d3ac08e5648dadede84b1041732
Ibrahim <ibrahs1@uci.edu>
2024-10-26 15:26:21 -0700

    created scraper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,28 @@
 import re
 from urllib.parse import urlparse
-
+from bs4 import BeautifulSoup
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    if resp.status!=200:
+        return list()
+    html = resp.raw_response.content
+    # html_bytes = html.encode('utf-8')
+    res = []
+
+    soup = BeautifulSoup(html, 'html.parser')
+    urls = [link.get('href') for link in soup.find_all('a') if link.get('href')]
+    link_urls = [link.get('href') for link in soup.find_all('link') if link.get('href')]
+    allURLS = urls+link_urls
+    for url in allURLS:
+        if is_valid(url):
+            res.append(url)
+
+    return res    
+
+
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -18,11 +35,23 @@ def extract_next_links(url, resp):
     return list()
 
 def is_valid(url):
+    validUrls = set([".cs.uci.edu/",
+        ".ics.uci.edu/",
+        ".informatics.uci.edu/",
+        ".stat.uci.edu/",
+        "today.uci.edu/department/information_computer_sciences/"
+     ])
+
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
+        stringUrl = parsed.netloc+parsed.path
+        for validUrl in validUrls:
+            if validUrl in stringUrl:
+                return True  
+                
         if parsed.scheme not in set(["http", "https"]):
             return False
         return not re.match(
